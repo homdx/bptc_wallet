629f295dd72914706a1b45edb80878b845e80385
Thomas Kellermeier <t-kellermeier@hotmail.de>
2017-06-14 15:46:39 +0200

    Fix argsparser to work parallel with kivy argsparser

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import os
 import argparse
-
 from bptc.utils import init_logger
-from bptc.client.mobile import MobileApp
 
 __version__ = '0.1'
 
 def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--port', type=int, default=8000,
-                        help='Porting for pulling information from other members and the registry')
-    parser.add_argument('-o', '--output', type=str, default='data',
-                        help='Output directory for the sqlite3 database and log files')
-    return parser.parse_args()
-
+    parser = argparse.ArgumentParser(prog='main.py', add_help=False)
+    group = parser.add_argument_group('Hashgraph Options')
+    group.add_argument('-ah', '--help-app', action='store_true',
+        help='Show this help message and exit')
+    group.add_argument('-po', '--port', type=int, default=8000,
+        help='Porting for pulling information from other members and the registry')
+    group.add_argument('-o', '--output', type=str, default='data',
+        help='Output directory for the sqlite3 database and log files')
+    args, _ = parser.parse_known_args()
+    if args.help_app:
+        parser.print_help()
+        parser.exit()
+    return args
 
 if __name__ == '__main__':
     # Right now there is only one app designed for mobile devives
     cl_args = parse_args()
+    from bptc.client.mobile import MobileApp
     os.makedirs(cl_args.output, exist_ok=True)
     init_logger(cl_args.output)
     MobileApp(cl_args).run()
