5e37393ad439ff671d23e4e1957b570cfc7c3b28
ceddie <cedric.freiberger@student.hpi.de>
2017-06-15 22:48:23 +0200

    put core consensus functions together

---
+++ bptc/data/consensus.py
@@ -0,0 +1,184 @@
+from collections import defaultdict
+from functools import reduce
+
+from libnacl.encode import base64_decode
+from toposort import toposort_flatten
+
+from bptc.utils import bfs
+
+C = 6  # How often a coin round occurs, e.g. 6 for every sixth round
+
+
+def divide_rounds(hashgraph, events):
+    """Restore invariants for `can_see`, `witnesses` and `round`.
+
+    :param hashgraph: Hashgraph
+    :param events: Topologically sorted sequence of new event to process.
+    """
+
+    graph = {}
+    for event_id, event in events.items():
+        parents = set()
+        if event.parents.self_parent:
+            parents.add(event.parents.self_parent)
+        if event.parents.other_parent:
+            parents.add(event.parents.other_parent)
+        graph[event.id] = parents
+
+    for event_id in reversed(list(toposort_flatten(graph))):
+        event = hashgraph.lookup_table[event_id]
+        # Check if this is a root event or not
+        if event.parents == (None, None):
+            # This is a root event
+            event.round = 0
+            hashgraph.witnesses[0][event.verify_key] = event
+            event.can_see = {event.verify_key: event}
+        else:
+            # This is a normal event
+            # Estimate round (= maximum round of parents)
+            # utils.logger.info("Checking {}".format(str(event.parents)))
+            calculated_round = 0
+            for parent in event.parents:
+                if parent is not None and hashgraph.lookup_table[parent].round > calculated_round:
+                    calculated_round = hashgraph.lookup_table[parent].round
+
+            # recurrence relation to update can_see
+            p0 = hashgraph.lookup_table[
+                event.parents.self_parent].can_see if event.parents.self_parent is not None else dict()
+            p1 = hashgraph.lookup_table[
+                event.parents.other_parent].can_see if event.parents.other_parent is not None else dict()
+            event.can_see = {c: hashgraph.get_higher(p0.get(c), p1.get(c))
+                             for c in p0.keys() | p1.keys()}
+
+            # Count distinct paths to distinct nodes
+            # TODO: What exactly happens here? Why two levels of visible events?
+            hits = defaultdict(int)
+            for verify_key, visible_event in event.can_see.items():
+                if visible_event.round == calculated_round:
+                    for c_, k_ in visible_event.can_see.items():
+                        if k_.round == calculated_round:
+                            hits[c_] += hashgraph.known_members[verify_key].stake
+
+            # check if i can strongly see enough events
+            if sum(1 for x in hits.values() if x > hashgraph.supermajority_stake) > hashgraph.supermajority_stake:
+                event.round = calculated_round + 1
+            else:
+                event.round = calculated_round
+
+            # Events can always see themselves
+            event.can_see[event.verify_key] = event
+
+            # An event becomes a witness if it is the first of that round
+            if event.round > hashgraph.lookup_table[event.parents.self_parent].round:
+                hashgraph.witnesses[event.round][event.verify_key] = event
+
+
+def decide_fame(hashgraph):
+    max_r = max(hashgraph.witnesses or [0])
+    max_c = 0
+    while max_c in hashgraph.consensus:
+        max_c += 1
+
+    # helpers to keep code clean
+    def iter_undetermined(r_):
+        for r in range(max_c, r_):
+            if r not in hashgraph.consensus:
+                for w in hashgraph.witnesses[r].values():
+                    if w not in hashgraph.famous:
+                        yield r, w
+
+    def iterate_witnesses():
+        """
+        A Generator for all witnesses that need to be handled
+        :return: A Generator for all witnesses that need to be handled
+        """
+        # For every round that has to be handled ...
+        for round in range(max_c + 1, max_r + 1):
+            # ... iterate over the witnesses of that round ...
+            for witness in hashgraph.witnesses[round].values():
+                # ... and yield (round, witness)
+                yield round, witness
+
+    done = set()
+
+    # Iterate over all unhandled witnesses
+    for round, witness in iterate_witnesses():  # type: int, Event
+
+        hits = defaultdict(int)
+        for c, k in witness.can_see.items():
+            if k.round == round - 1:
+                for c_, k_ in k.can_see.items():
+                    if k_.round == round - 1:
+                        hits[c_] += hashgraph.known_members[c].stake
+        s = {hashgraph.witnesses[round - 1][c] for c, n in hits.items()
+             if n > hashgraph.supermajority_stake}
+
+        for r, x in iter_undetermined(round):
+            if round - r == 1:
+                witness.votes[x] = x in s
+            else:
+                filtered_s = filter(lambda w: w in hashgraph.lookup_table, s)  # TODO: check why filtering is necessary
+                v, t = majority((hashgraph.known_members[hashgraph.lookup_table[w].verify_key].stake, w.votes[x]) for w in filtered_s)
+                if (round - r) % C != 0:
+                    if t > hashgraph.supermajority_stake:
+                        hashgraph.famous[x] = v
+                        done.add(r)
+                    else:
+                        witness.votes[x] = v
+                else:
+                    if t > hashgraph.supermajority_stake:
+                        witness.votes[x] = v
+                    else:
+                        # the 1st bit is same as any other bit right? # TODO not!
+                        witness_signature_byte = base64_decode(witness.signature.encode("UTF-8"))
+                        witness.votes[x] = bool(witness_signature_byte[0] // 128)
+
+    new_c = {r for r in done
+             if all(w in hashgraph.famous for w in hashgraph.witnesses[r].values())}
+    hashgraph.consensus |= new_c
+    return new_c
+
+
+def find_order(self, new_c):
+    def to_int(x):
+        return int.from_bytes(self.lookup_table[x].signature, byteorder='big')
+
+    for r in sorted(new_c):
+        f_w = {w for w in self.witnesses[r].values() if self.famous[w]}
+        white = reduce(lambda a, b: a ^ to_int(b), f_w, 0)
+        ts = {}
+        seen = set()
+        for x in bfs(filter(self.unordered_events.__contains__, f_w),
+                     lambda u: (p for p in self.lookup_table[u].parents if p in self.unordered_events)):
+            c = self.lookup_table[x].verify_key
+            s = {w for w in f_w if c in w.can_see
+                 and self.is_higher(w.can_see[c], x)}
+            if sum(self.known_members[self.lookup_table[w].verify_key].stake for w in s) > self.total_stake / 2:
+                self.unordered_events.remove(x)
+                seen.add(x)
+                times = []
+                for w in s:
+                    a = w
+                    while (c in a.can_see
+                           and self.is_higher(a.can_see[c], x)
+                           and self.lookup_table[a].parents):
+                        a = self.lookup_table[a].p[0]
+                    times.append(self.lookup_table[a].t)
+                times.sort()
+                ts[x] = .5 * (times[len(times) // 2] + times[(len(times) + 1) // 2])
+        final = sorted(seen, key=lambda x: (ts[x], white ^ to_int(x)))
+        for i, x in enumerate(final):
+            self.idx[x] = i + len(self.ordered_events)
+        self.ordered_events += final
+    if self.consensus:
+        print(self.consensus)
+
+
+def majority(it):
+    hits = [0, 0]
+    for s, x in it:
+        hits[int(x)] += s
+    if hits[0] > hits[1]:
+        return False, hits[0]
+    else:
+        return True, hits[1]
--- bptc/data/hashgraph.py
+++ bptc/data/hashgraph.py
@@ -1,18 +1,12 @@
 import math
 from collections import defaultdict
-from functools import reduce, partial
-from typing import List, Dict
-
-from libnacl.encode import base64_decode
-
-from bptc.data import hg_algorithm
+from functools import partial
+from typing import Dict
+from bptc.data import consensus
 from bptc.data.event import Event, Parents
 from bptc.data.member import Member
 from bptc.utils import bfs
 import bptc.utils as utils
-
-
-C = 6  # How often a coin round occurs, e.g. 6 for every sixth round
 
 
 class Hashgraph:
@@ -48,7 +42,9 @@ class Hashgraph:
         self.famous = {}
 
         # add functions of hashgraph algorithm
-        self.divide_rounds = partial(hg_algorithm.divide_rounds, self)
+        self.divide_rounds = partial(consensus.divide_rounds, self)
+        self.decide_fame = partial(consensus.decide_fame, self)
+        self.find_order = partial(consensus.find_order, self)
 
     @property
     def total_stake(self) -> int:
@@ -123,8 +119,6 @@ class Hashgraph:
         new_c = self.decide_fame()
         self.find_order(new_c)
 
-        #utils.logger.info("Added own event to hashgraph: " + str(event))
-
     @staticmethod
     def get_fingerprint(member: Member):
         """Returns dict of heights for each member."""
@@ -181,105 +175,6 @@ class Hashgraph:
         :return: boolean: Whether a is higher than b
         """
         return a is not None and (b is None or a.height >= b.height)
-
-    def decide_fame(self):
-        max_r = max(self.witnesses or [0])
-        max_c = 0
-        while max_c in self.consensus:
-            max_c += 1
-
-        # helpers to keep code clean
-        def iter_undetermined(r_):
-            for r in range(max_c, r_):
-                if r not in self.consensus:
-                    for w in self.witnesses[r].values():
-                        if w not in self.famous:
-                            yield r, w
-
-        def iterate_witnesses():
-            """
-            A Generator for all witnesses that need to be handled
-            :return: A Generator for all witnesses that need to be handled
-            """
-            # For every round that has to be handled ...
-            for round in range(max_c + 1, max_r + 1):
-                # ... iterate over the witnesses of that round ...
-                for witness in self.witnesses[round].values():
-                    # ... and yield (round, witness)
-                    yield round, witness
-
-        done = set()
-
-        # Iterate over all unhandled witnesses
-        for round, witness in iterate_witnesses():  # type: int, Event
-
-            hits = defaultdict(int)
-            for c, k in witness.can_see.items():
-                if k.round == round - 1:
-                    for c_, k_ in k.can_see.items():
-                        if k_.round == round - 1:
-                            hits[c_] += self.known_members[c].stake
-            s = {self.witnesses[round - 1][c] for c, n in hits.items()
-                 if n > self.supermajority_stake}
-
-            for r, x in iter_undetermined(round):
-                if round - r == 1:
-                    witness.votes[x] = x in s
-                else:
-                    filtered_s = filter(lambda w: w in self.lookup_table, s)  # TODO: check why filtering is necessary
-                    v, t = majority((self.known_members[self.lookup_table[w].verify_key].stake, w.votes[x]) for w in filtered_s)
-                    if (round - r) % C != 0:
-                        if t > self.supermajority_stake:
-                            self.famous[x] = v
-                            done.add(r)
-                        else:
-                            witness.votes[x] = v
-                    else:
-                        if t > self.supermajority_stake:
-                            witness.votes[x] = v
-                        else:
-                            # the 1st bit is same as any other bit right? # TODO not!
-                            witness_signature_byte = base64_decode(witness.signature.encode("UTF-8"))
-                            witness.votes[x] = bool(witness_signature_byte[0] // 128)
-
-        new_c = {r for r in done
-                 if all(w in self.famous for w in self.witnesses[r].values())}
-        self.consensus |= new_c
-        return new_c
-
-    def find_order(self, new_c):
-        def to_int(x):
-            return int.from_bytes(self.lookup_table[x].signature, byteorder='big')
-
-        for r in sorted(new_c):
-            f_w = {w for w in self.witnesses[r].values() if self.famous[w]}
-            white = reduce(lambda a, b: a ^ to_int(b), f_w, 0)
-            ts = {}
-            seen = set()
-            for x in bfs(filter(self.unordered_events.__contains__, f_w),
-                         lambda u: (p for p in self.lookup_table[u].parents if p in self.unordered_events)):
-                c = self.lookup_table[x].verify_key
-                s = {w for w in f_w if c in w.can_see
-                     and self.is_higher(w.can_see[c], x)}
-                if sum(self.known_members[self.lookup_table[w].verify_key].stake for w in s) > self.total_stake / 2:
-                    self.unordered_events.remove(x)
-                    seen.add(x)
-                    times = []
-                    for w in s:
-                        a = w
-                        while (c in a.can_see
-                               and self.is_higher(a.can_see[c], x)
-                               and self.lookup_table[a].parents):
-                            a = self.lookup_table[a].p[0]
-                        times.append(self.lookup_table[a].t)
-                    times.sort()
-                    ts[x] = .5 * (times[len(times) // 2] + times[(len(times) + 1) // 2])
-            final = sorted(seen, key=lambda x: (ts[x], white ^ to_int(x)))
-            for i, x in enumerate(final):
-                self.idx[x] = i + len(self.ordered_events)
-            self.ordered_events += final
-        if self.consensus:
-            print(self.consensus)
 
     def process_events(self, from_member: Member, events: Dict[str, Event]) -> None:
         """
@@ -349,16 +244,6 @@ class Hashgraph:
         for event in events.values():
             if event.verify_key not in self.known_members:
                 self.known_members[event.verify_key] = Member(event.verify_key)
-
-
-def majority(it):
-    hits = [0, 0]
-    for s, x in it:
-        hits[int(x)] += s
-    if hits[0] > hits[1]:
-        return False, hits[0]
-    else:
-        return True, hits[1]
 
 
 def filter_valid_events(events: Dict[str, Event]) -> Dict[str, Event]:
