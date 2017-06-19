7f5251bf0781d03a4971254a93a6cca880293991
ceddie <cedric.freiberger@student.hpi.de>
2017-06-19 21:40:26 +0200

    added script for starting multiple clients

---
+++ simulation_data/2/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+var=8000
+for dir in *; do
+	if [[ -d $dir ]]; then
+		gnome-terminal --tab -e "/bin/bash -c 'cd $dir; python3 ../../../main.py -p $var; exec /bin/bash -i'"
+		var=$((var+2))
+	fi
+done
