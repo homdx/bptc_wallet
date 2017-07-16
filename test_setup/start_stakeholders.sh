a40ee8a71e16d36c4a14734703bed1d9db1b95cb
ceddie <cedric.freiberger@student.hpi.de>
2017-07-16 14:05:23 +0200

    changed test setup

---
+++ test_setup/start_stakeholders.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+port=8000
+
+for i in `seq 1 4`
+do
+    #rm -rf $i
+    #mkdir $i
+
+    ip='localhost'
+
+    # auto-close terminal
+    gnome-terminal --tab -e "/bin/bash -c 'cd stakeholders/$i; ../../../main.py -cli -q -ip $ip -p $port -sp'"
+
+    # don't auto-close terminal
+    #gnome-terminal --tab -e "/bin/bash -c 'cd stakeholders/$i; ../../../main.py -cli -q -ip $ip -p $port -sp; exec /bin/bash -i'"
+
+    port=$((port+2))
+done
