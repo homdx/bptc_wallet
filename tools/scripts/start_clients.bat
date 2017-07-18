224f6da44f9ebd10404f7fe2c69a5eb1a7f97aa2
Thomas Kellermeier <t-kellermeier@hotmail.de>
2017-07-18 11:22:45 +0200

    fix bat scripts

--- tools/scripts/start_clients.bat
+++ tools/scripts/start_clients.bat
@@ -1,8 +1,8 @@
 :: start .\tools\scripts\registry
-start .\tools\scripts\client 8100 "-sp -bp localhost:8102"
-start .\tools\scripts\client 8102 "-sp -bp localhost:8100"
-start .\tools\scripts\client 8104 "-sp -bp localhost:8100"
-start .\tools\scripts\client 8106 "-sp -bp localhost:8100"
+start .\tools\scripts\client 8100 "-bp localhost:8102"
+start .\tools\scripts\client 8102 "-bp localhost:8100"
+start .\tools\scripts\client 8104 "-bp localhost:8100"
+start .\tools\scripts\client 8106 "-bp localhost:8100"
 :: start .\tools\scripts\client 8108
 :: start .\tools\scripts\client 8110
 :: start .\tools\scripts\client 8112
--- tools/scripts/start_stakeholders.bat
+++ tools/scripts/start_stakeholders.bat
@@ -1,4 +1,4 @@
-start .\tools\scripts\stakeholder 8000 "-sp -o demo_setup\stakeholders\1\data"
-start .\tools\scripts\stakeholder 8002 "-sp -o demo_setup\stakeholders\2\data"
-start .\tools\scripts\stakeholder 8004 "-sp -o demo_setup\stakeholders\3\data"
-start .\tools\scripts\stakeholder 8006 "-sp -o demo_setup\stakeholders\4\data"
+start .\tools\scripts\stakeholder 8000 "-o demo_setup\stakeholders\1\data"
+start .\tools\scripts\stakeholder 8002 "-o demo_setup\stakeholders\2\data"
+start .\tools\scripts\stakeholder 8004 "-o demo_setup\stakeholders\3\data"
+start .\tools\scripts\stakeholder 8006 "-o demo_setup\stakeholders\4\data"
