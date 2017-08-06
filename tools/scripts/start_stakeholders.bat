a12b2dea94bbfef8e82ecacefcb0a73f87f5a809
Thomas Kellermeier <t-kellermeier@hotmail.de>
2017-08-06 13:55:23 +0200

    run stakeholders on windows

--- tools/scripts/start_stakeholders.bat
+++ tools/scripts/start_stakeholders.bat
@@ -1,3 +1,4 @@
+git checkout -- demo_setup
 start .\tools\scripts\stakeholder 8000 "-o demo_setup\stakeholders\1\data"
 start .\tools\scripts\stakeholder 8002 "-o demo_setup\stakeholders\2\data"
 start .\tools\scripts\stakeholder 8004 "-o demo_setup\stakeholders\3\data"
