26ee2bd042379816add41211c29c16d6f1e59837
Thomas Kellermeier <t-kellermeier@hotmail.de>
2017-08-07 01:13:33 +0200

    docker-compose for demo setup

--- tools/scripts/start_stakeholders.bat
+++ tools/scripts/start_stakeholders.bat
@@ -1,5 +1,5 @@
 git checkout -- demo_setup
-start .\tools\scripts\stakeholder 8000 "-o demo_setup\stakeholders\1\data"
-start .\tools\scripts\stakeholder 8002 "-o demo_setup\stakeholders\2\data"
-start .\tools\scripts\stakeholder 8004 "-o demo_setup\stakeholders\3\data"
-start .\tools\scripts\stakeholder 8006 "-o demo_setup\stakeholders\4\data"
+start .\tools\scripts\stakeholder 8000 "-o demo_setup\stakeholders\1\data -bp localhost:8002"
+start .\tools\scripts\stakeholder 8002 "-o demo_setup\stakeholders\2\data -bp localhost:8004"
+start .\tools\scripts\stakeholder 8004 "-o demo_setup\stakeholders\3\data -bp localhost:8006"
+start .\tools\scripts\stakeholder 8006 "-o demo_setup\stakeholders\4\data -bp localhost:8000"
